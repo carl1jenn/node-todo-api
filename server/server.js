9c67c6e12077199d3cc8aa1fc2a7e64ac14749a2
Carl Eastman <carl1jenn@gmail.com>
2018-04-11 07:31:45 -0500

    ''

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,6 +31,33 @@ app.get('/todos', (req,res) => {
   })
 });
 
+// get /todos/123343
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+    return res.status(200).send({todo});
+    console.log('todo by id', todo);
+  }).catch((e) => console.log(e));
+
+
+  //valid id is using isValid
+    // 404 - send back empty send
+
+  // findById
+    // success
+      // if todo send it back
+      // if no todo - send back 404 with empty body
+    // error
+      // 400 -
+})
 
 app.listen(3000, () => {
   console.log('started on port 3000');
