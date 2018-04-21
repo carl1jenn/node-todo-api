c98b3585f2cc9310be174c7925703f77cd0bc2b2
Carl Eastman <carl1jenn@gmail.com>
2018-04-21 15:11:54 -0500

    setup separate test and dev envs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,9 @@ const todos = [{
   text: 'first test todo'
 },{
   _id: new ObjectID(),
-  text: 'second test todo'
+  text: 'second test todo',
+  completed: true,
+  completedAt: 232
 }];
 
 beforeEach((done) => {
@@ -132,5 +134,41 @@ describe('delete /todos/:id', () => {
     .delete('/todos/123')
     .expect(404)
     .end(done);
+  });
+});
+
+describe('patch /todos/:id', () => {
+  it('should update the todo', (done) => {
+      var hexId = todos[0]._id.toHexString();
+      var text = 'updated from test case';
+      request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+      })
+      .end(done)
+  });
+
+  it('should clear the completed flag', (done) => {
+      var hexId = todos[1]._id.toHexString();
+      var text = 'updated from test case2';
+      request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+      })
+      .end(done)
   });
 });
